dab9d433e445c956df2658836e26e5f271f1202a
FelipePi27 <f.pizarroromo@gmail.com>
2022-09-10 16:23:04 -0300

    commit 4

---
+++ SuperHeroes/DATOS/CategoriaRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using SuperHeroes.NEGOCIO;
+using System.Data.SqlClient;
+
+namespace SuperHeroes.DATOS
+{
+    public class CategoriaRepositorio
+    {
+        private readonly IConfiguration _configuration;
+
+        public CategoriaRepositorio(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public List<Categoria> ObtenerTodas() 
+        {
+            var ListaCategorias = new List<Categoria>();
+            using SqlConnection sql = new SqlConnection(_configuration.GetConnectionString("conexionPorDefecto"));
+            using SqlCommand cmd = new SqlCommand("select * from Categorias", sql);
+            sql.Open();
+            using(var reader = cmd.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    var nuevaCategoria = new Categoria {Id =(int)reader["Id"],Nombre = reader["Nombre"].ToString() };
+                    ListaCategorias.Add(nuevaCategoria);    
+                }
+            }
+
+
+            return ListaCategorias;
+        
+        }
+       
+            
+
+    }
+}
